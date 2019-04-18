344be86063222288cfda6cd044bf6b3addf03cd6
Johan Rios <jsriosg@unal.edu.co>
2019-04-18 00:00:18 -0500

    corrijo ortografia

--- hola.cpp
+++ hola.cpp
@@ -1,6 +1,6 @@
 #include <iostream>
 
 int main(int argc, char const *argv[]) {
-  std::cout <<"hola mundo, prueba diferente branch, funcion batante bien"<<std::endl;
+  std::cout <<"hola mundo, prueba diferente branch, funciona batante bien"<<std::endl;
   return 0;
 }
