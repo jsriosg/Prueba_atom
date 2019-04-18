aabada532aec0640b84de2b33cd326cc9e3ab203
Johan Rios <jsriosg@unal.edu.co>
2019-04-17 23:29:51 -0500

    version personal hola mundo

--- hola.cpp
+++ hola.cpp
@@ -1,6 +1,6 @@
 #include <iostream>
 
 int main(int argc, char const *argv[]) {
-  std::cout <<"hola mundo"<<std::endl;
+  std::cout <<"hola mundo, prueba diferente branch"<<std::endl;
   return 0;
 }
